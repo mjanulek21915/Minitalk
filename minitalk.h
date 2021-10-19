86a7ffcd4e267b5960094e4d7484f7e19b878a9e
mjanulek <aaa>
2021-10-19 13:52:59 +0200

    Minitalk

--- minitalk.h
+++ minitalk.h
@@ -10,18 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef __MINITALK_H__
-#define __MINITALK_H__
+#ifndef MINITALK_H
+ #define MINITALK_H
 
-# include <signal.h>
-# include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <limits.h>
-void				do_exit(void)
+ #include <signal.h>
+ #include <unistd.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <limits.h>
+
 void				ft_listen(int sig, siginfo_t *info, void *context);
 void				ft_write_pid(pid_t nbr);
 unsigned int		ft_atoi(char *str);
-void				do_exit(void)
+void				do_exit(void);
 
 #endif
